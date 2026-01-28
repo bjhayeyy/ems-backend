9cdbde4c87b3ec403267d04afdb19d49b02df483
bjhayeyy <bjhayeyy@gmail.com>
2026-01-29 07:11:50 +0800

    get single employee

--- src/main/java/net/javaguides/ems_backend/service/impl/EmployeeServiceImpl.java
+++ src/main/java/net/javaguides/ems_backend/service/impl/EmployeeServiceImpl.java
@@ -3,6 +3,7 @@ package net.javaguides.ems_backend.service.impl;
 import lombok.AllArgsConstructor;
 import net.javaguides.ems_backend.dto.EmployeeDto;
 import net.javaguides.ems_backend.entity.Employee;
+import net.javaguides.ems_backend.exception.ResourceNotFoundException;
 import net.javaguides.ems_backend.mapper.EmployeeMapper;
 import net.javaguides.ems_backend.repository.EmployeeRepository;
 import net.javaguides.ems_backend.service.EmployeeService;
@@ -20,5 +21,13 @@ public class EmployeeServiceImpl implements EmployeeService {
         Employee savedEmployee = employeeRepository.save(employee);
 
         return EmployeeMapper.mapToEmployeeDto(savedEmployee);
+    }
+
+    @Override
+    public EmployeeDto getEmployeeById(Long employeeId) {
+        Employee employee = employeeRepository.findById(employeeId)
+                .orElseThrow(() ->
+                        new ResourceNotFoundException("Employee does not exists  with the given id : " + employeeId));
+        return EmployeeMapper.mapToEmployeeDto(employee);
     }
 }
