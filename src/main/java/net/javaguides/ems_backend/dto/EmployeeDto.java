fb8e52d8cc1d95c2aa501d238cbee28f5f7f72dc
bjhayeyy <bjhayeyy@gmail.com>
2026-01-29 06:57:51 +0800

    Initial commit

---
+++ src/main/java/net/javaguides/ems_backend/dto/EmployeeDto.java
@@ -0,0 +1,14 @@
+package net.javaguides.ems_backend.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class EmployeeDto {
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+}
